6bbdc0fe8f6e89c52977aaa6e898dc0a1ef49002
sindresrhus-sys <sindre.srhus@gmail.com>
2026-09-17 19:25:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='spekkhoggerkart-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
